f0623b35512b7981b744884162da501401ca13fe
daboss2003 <samsonoluwafemi@gmail.com>
2024-03-12 12:08:10 +0100

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "03342d44-5a6a-4ad3-be20-cba780faf6f1";
+export const MODAL_CLOSE_SEC = 2.5;
